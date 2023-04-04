a1e7230d6d85f0f222d5d5093d6ee702df4b6129
ndrbr <andrey.burdak@gmail.com>
2023-04-04 17:06:59 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+// const express = require("express");
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send({
+    id: 1,
+    name: "Sir Isaac Newton",
+  });
+});
+
+app.get("/messages", (req, res) => {
+  res.send("<ul><li>Privit!</li></ul>");
+});
+
+app.post("/messages", (req, res) => {
+  console.log("Updating messages...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}...`);
+});
